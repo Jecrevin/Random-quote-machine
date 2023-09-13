8b98358c30299aae4207aa023a84b1e4f5e4b303
Jecrevin <1838821372@qq.com>
2023-09-13 10:30:48 +0800

    fit ESLint

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
-import Link from "next/link";
-import QuoteBox from "./quoteBox";
-import { Work_Sans } from "next/font/google";
-import { getQuotes, getRamdomQuote, getRandomColor } from "./source";
+import React from 'react'
+import Link from 'next/link'
+import QuoteBox from './quoteBox'
+import { Work_Sans } from 'next/font/google'
+import { getQuotes, getRamdomQuote, getRandomColor } from './source'
 
 const workSans = Work_Sans({
-  subsets: ["latin"]
+  subsets: ['latin']
 })
 
-export default async function Home() {
-  const initColor = getRandomColor();
+export default async function Home (): Promise<React.JSX.Element> {
+  const initColor = getRandomColor()
   try {
-    const quotes = await getQuotes();
-    const firstQuote = getRamdomQuote(quotes);
+    const quotes = await getQuotes()
+    const firstQuote = getRamdomQuote(quotes)
     return (
       <main className="w-screen h-screen flex justify-center items-center flex-col"
         id="background" style={{ backgroundColor: initColor }}>
         <QuoteBox quotes={quotes} quote={firstQuote[0]} author={firstQuote[1]}
           initColor={initColor} />
-        <p id="web-author" className={"text-white" + ' ' + workSans.className}>
-          by <Link href={"https://www.freecodecamp.org/chinese/Jecrevin"}
+        <p id="web-author" className={'text-white' + ' ' + workSans.className}>
+          by <Link href={'https://www.freecodecamp.org/chinese/Jecrevin'}
             id="author-link" className="font-medium">Jecrevin</Link>
         </p>
       </main>
--- app/source.tsx
+++ app/source.tsx
@@ -1,46 +1,46 @@
 export const colors = [
-  "#6b7280",
-  "#57534e",
-  "#ef4444",
-  "#fb923c",
-  "#fcd34d",
-  "#facc15",
-  "#a3e635",
-  "#22c55e",
-  "#22d3ee",
-  "#0ea5e9",
-  "#2563eb",
-  "#8b5cf6",
-  "#7e22ce",
-  "#ec4899",
-  "#f43f5e"
+  '#6b7280',
+  '#57534e',
+  '#ef4444',
+  '#fb923c',
+  '#fcd34d',
+  '#facc15',
+  '#a3e635',
+  '#22c55e',
+  '#22d3ee',
+  '#0ea5e9',
+  '#2563eb',
+  '#8b5cf6',
+  '#7e22ce',
+  '#ec4899',
+  '#f43f5e'
 ]
 
-export function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+export function getRandomColor (): string {
+  return colors[Math.floor(Math.random() * colors.length)]
 }
 
 /**
  * Fetch quotes data from website.
  * @returns An array containing objects, each object is made of quote and its author.
  */
-export async function getQuotes() {
-  const response = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json");
+export async function getQuotes (): Promise<Array<{ quote: string, author: string }>> {
+  const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
   if (!response.ok) {
-    throw new Error();
+    throw new Error()
   }
-  const quotesData: { quotes: { quote: string, author: string }[] }
-    = await response.json();
-  return quotesData.quotes;
+  const quotesData: { quotes: Array<{ quote: string, author: string }> } =
+    await response.json()
+  return quotesData.quotes
 }
 
 /**
- * 
+ *
  * @param quotesData An array containing quotes, which is from the function getQuotes()
  * @returns a two element array where the first is quote,
  *          the other is the corresonding author.
  */
-export function getRamdomQuote(quotesData: Array<{ quote: string, author: string }>) {
-  const index = Math.floor(Math.random() * quotesData.length);
-  return [quotesData[index].quote, quotesData[index].author];
+export function getRamdomQuote (quotesData: Array<{ quote: string, author: string }>): string[] {
+  const index = Math.floor(Math.random() * quotesData.length)
+  return [quotesData[index].quote, quotesData[index].author]
 }
